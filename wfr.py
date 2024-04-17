ef14b194af64d18c7a4c4d848a7405a86ec2c9ee
aosman001 <aosman001@outlook.com>
2024-04-17 17:00:00 -0400

    camera files

---
+++ wfr.py
@@ -0,0 +1,76 @@
+from pymavlink import mavutil
+import threading
+import time
+
+# mavlink_monitor edits these global variables that are consumed by the other threads, like state.
+walking_lock = True
+walking_direction = "middle" # "forward" | "middle" | "back"
+walking_speed = 0.0 # 0.0 to 1.0
+
+# Reads controls from the Flysky
+def mavlink_monitor():
+  global walking_speed, walking_direction, walking_lock
+  master = mavutil.mavlink_connection("/dev/ttyAMA0", baud=57_600)
+  running = True
+  while running:
+    try:
+      msg = master.recv_match(type="RC_CHANNELS", blocking=True)
+      if not msg:
+        print("ERROR: msg was NONE somehow")
+        return
+      # print(f"Channel 1: {msg.chan1_raw}")
+      # print(f"Channel 2: {msg.chan2_raw}")
+      # print(f"Channel 3: {msg.chan3_raw}")
+      # print(f"Channel 4: {msg.chan4_raw}")
+      # print(f"Channel 5: {msg.chan5_raw}")
+      # print(f"Channel 6: {msg.chan6_raw}")
+      # print(f"Channel 7: {msg.chan7_raw}")
+      # print(f"Channel 8: {msg.chan8_raw}")
+
+      # Values are empirical
+      walking_speed_channel = msg.chan6_raw
+      walking_dir_channel = msg.chan7_raw
+      walking_lock_channel = msg.chan8_raw
+
+      walking_speed = max(0.0, min(1.0, (walking_speed_channel - 1000) / 1000))
+
+      if walking_dir_channel < 1100:
+        walking_direction = "forward"
+      elif walking_dir_channel >= 1100 and walking_dir_channel < 1600:
+        walking_direction = "middle"
+      else:
+        walking_direction = "back"
+
+      if walking_lock_channel < 1500:
+        walking_lock = False
+      else:
+        walking_lock = True
+
+    except KeyboardInterrupt:
+      print("Exiting...")
+      running = False
+    except Exception as e:
+      print("------EXCEPTION--------")
+  
+  master.close()
+
+
+def control_walking():
+  global walking_speed, walking_direction, walking_lock
+  while True:
+    print(walking_speed, walking_direction, walking_lock)
+    time.sleep(0.1)
+  
+
+if __name__ == "__main__":
+  # Thread the functions to run both at the same time independent of one another
+  mavlink_thread = threading.Thread(target=mavlink_monitor)
+  mavlink_thread.daemon = True
+  mavlink_thread.start()
+
+  control_thread = threading.Thread(target=control_walking)
+  control_thread.daemon = True
+  control_thread.start()
+
+  mavlink_thread.join()  # Add this line to make the main thread wait for this thread to finish
+  control_thread.join()  # Add this line to make the main thread wait for this thread to finish
