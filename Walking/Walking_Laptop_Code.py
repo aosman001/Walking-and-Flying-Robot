4a48947ee1cc8c75885a207c9eaa6ea6e4859147
Alaa <alaa@Admins-MacBook-Pro-2.local>
2024-04-18 19:33:00 -0400

    updated laptop code

--- Walking/Walking_Laptop_Code.py
+++ Walking/Walking_Laptop_Code.py
@@ -22,7 +22,9 @@ import threading            # To run different sections of code concurently.
 
 # Initialize the servo controller, connect to the servos, and set their angle limits.
 
-LX16A.initialize("/dev/cu.usbserial-1430", 0.1)
+#LX16A.initialize("/dev/cu.usbserial-1430", 0.1)
+LX16A.initialize("COM5", 0.1)
+
 
 try:
     servo1 = LX16A(1)
@@ -75,16 +77,16 @@ def stand():
     print("Standing.")
 
     ### Upper motors.
-    servo1.move(37.20, 1000)
+    servo1.move(26, 1000) #37.20
     servo3.move(147.84, 1000)
     servo5.move(118.32, 1000)
-    servo7.move(117, 1000)
+    servo7.move(92, 1000)
     time.sleep(1.5)
     ### Lower motors.
-    servo2.move(150.96, 1000)
+    servo2.move(167, 1000)
     servo4.move(108, 1000)
     servo6.move(50.88, 1000)
-    servo8.move(106, 1000)
+    servo8.move(110, 1000) #108
     time.sleep(2)
 
 # Make the robot tuck its legs undenreath the body.
@@ -137,10 +139,10 @@ def walk(walking_lock = False, walking_direction = 'forward', walking_speed=1):
                 print('Moving forward')
                 while True:
                     positions = [
-                        (50, 150, 197, 24, 108, 42, 122, 105),  # Step 1
-                        (37, 130, 197, 99, 118, 65, 122, 55),   # Step 2
-                        (75, 81, 168, 90, 130, 56, 85, 90),     # Step 3
-                        (75, 152, 157, 73, 130, 10, 95, 112),   # Step 4
+                        (50, 150, 197, 24, 108, 50, 110, 114),  # Step 1
+                        (37, 130, 197, 99, 118, 65, 110, 68),   # Step 2
+                        (75, 81, 168, 90, 146, 56, 82, 110),     # Step 3
+                        (75, 152, 157, 73, 146, 10, 92, 125),   # Step 4
                     ]
                     for pos in positions:
                         try:
@@ -174,6 +176,33 @@ def walk(walking_lock = False, walking_direction = 'forward', walking_speed=1):
             elif walking_direction == 'backward':
 
                 print('Moving backward--still in progress!') # We still need to figure this out.
+                print('Moving forward')
+                while True:
+                    positions = [
+                        (40,143,185,80,122,55,124,50),    # Step 1
+                        (50,158,185,34,109,35,124,125),   # Step 2
+                        (78,149,157,76,147,0,95,114),        # Step 3
+                        (78,103,167,91,147,57,84,97),       # Step 4
+                    ]
+                    for pos in positions:
+                        try:
+                            servo1.move(pos[0], int(1000/walking_speed))
+                            servo2.move(pos[1], int(1000/walking_speed))
+                            servo3.move(pos[2], int(1000/walking_speed))
+                            servo4.move(pos[3], int(1000/walking_speed))
+                            servo5.move(pos[4], int(1000/walking_speed))
+                            servo6.move(pos[5], int(1000/walking_speed))
+                            servo7.move(pos[6], int(1000/walking_speed))
+                            servo8.move(pos[7], int(1000/walking_speed))
+
+                            time.sleep(0.8/walking_speed)
+
+                            if stop_flag:
+                                return
+                        except KeyboardInterrupt:
+                            stop_flag = True
+                            print("Walk interrupted by user.")
+                            return
 
         except Exception as e:
             print(f"An error occurred: {e}")
