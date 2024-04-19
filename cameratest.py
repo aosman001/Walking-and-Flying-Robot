1ba9036f21a25b33b42978f68a261dc4365dfc1f
aosman001 <aosman001@outlook.com>
2024-04-18 20:36:35 -0400

    Camera thread every 10 seconds

--- cameratest.py
+++ cameratest.py
@@ -1,23 +1,13 @@
 from picamera2 import Picamera2, Preview
 import libcamera
 import time
-import os
-
 picam2 = Picamera2()
 camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
 # Add transform to rotate the image by 180 degrees
 camera_config["transform"] = libcamera.Transform(hflip=1, vflip=1)
 picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
 picam2.start()
-
-
-
-def camera_main():
-  while True:
-    timestamp = time.strftime("%Y%m%d_%H%M%S")
-    filename = f"wfr_{timestamp}.jpg"
-    folder_path = "images"
-    file_path = os.path.join(folder_path, filename)
-    picam2.capture_file(file_path)
-    print(f"Picture taken! Filename: {file_path}")
-    time.sleep(10)
+# time.sleep(2)
+# picam2.capture_file("test.jpg")
+time.sleep(60)
